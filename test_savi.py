e866872448bc28d63a4bbae9e41c2792401fb8f3
Arif <arif@themascoteers.com>
2024-03-01 16:52:43 +1100

    done

--- test_savi.py
+++ test_savi.py
@@ -11,12 +11,25 @@ if __name__ == "__main__":
         algorithms=[
             "ann_savi",
             "ann_savi_learnable",
+            "ann_savi_learnable_half",
+
             "ann_savi_skip",
             "ann_savi_skip_learnable",
+            "ann_savi_skip_learnable_half",
+
             "ann_savi_skip_all",
             "ann_savi_skip_all_learnable",
-            "ann_savi_bands_only",
-            "ann_savi_learnable_fn"
+            "ann_savi_skip_all_learnable_half",
+
+            "ann_savi_learnable_fn",
+            "ann_savi_learnable_fn_all",
+            "ann_savi_learnable_bi",
+
+            "ann_savi_learnable_fn_skip",
+            "ann_savi_learnable_fn_all_skip",
+            "ann_savi_learnable_bi_skip",
+
+            "ann_savi_bands_only"
         ],
         feature_sets= [
             []
