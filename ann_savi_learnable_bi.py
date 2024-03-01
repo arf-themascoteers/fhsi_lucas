1d8e27917313b295e47f4e7a061fa1c7a39decf1
Arif <arif@themascoteers.com>
2024-03-01 14:41:26 +1100

    done

--- ann_savi_learnable_bi.py
+++ ann_savi_learnable_bi.py
@@ -13,8 +13,13 @@ class ANNSAVILearnableBI(ANNSAVI):
         )
 
     def get_L(self, x=None):
+        return torch.mean(self.linear_l(self.get_bi(x)))
+
+    def get_bi(self, x):
         band_3 = x[:, 2]
         band_4 = x[:, 3]
-        bi = torch.sqrt((torch.square(band_3) + torch.square(band_4)) / 2).reshape(x.shape[0], 1)
-        return torch.mean(self.linear_l(bi))
+        self.bi = torch.sqrt((torch.square(band_3) + torch.square(band_4)) / 2).reshape(x.shape[0], 1)
+        return self.bi
+
+
 
--- ann_savi_learnable_fn_skip.py
+++ ann_savi_learnable_fn_skip.py
@@ -1,17 +1,15 @@
 import torch
 import torch.nn as nn
-from ann_savi_learnable_bi import ANNSAVILearnableBI
+from ann_savi_learnable_fn import ANNSAVILearnableFn
 
 
-class ANNSAVILearnableBISkip(ANNSAVILearnableBI):
+class ANNSAVILearnableFnSkip(ANNSAVILearnableFn):
     def __init__(self, train_ds, test_ds, validation_ds):
         super().__init__(train_ds, test_ds, validation_ds)
-        self.linear = nn.Sequential(
-            nn.Linear(4,20),
+        self.linear1 = nn.Sequential(
+            nn.Linear(3,20),
             nn.LeakyReLU(),
-            nn.Linear(20, 10),
-            nn.LeakyReLU(),
-            nn.Linear(10,1)
+            nn.Linear(20, 1)
         )
 
     def forward(self,x):
@@ -19,5 +17,9 @@ class ANNSAVILearnableBISkip(ANNSAVILearnableBI):
         band_8 = x[:,7:8]
         band_4 = x[:,3:4]
         x_short = torch.hstack((band_4, band_8))
-        x = torch.hstack((x_short, savi_val, self.bi))
+        x = torch.hstack((x_short, savi_val))
         return self.linear(x)
+
+
+
+
